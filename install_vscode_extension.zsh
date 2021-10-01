9e57c53ab76eaa2aaff958188d01fa2fcbe3f96a
peterhpchen <peterhsinpingchen@gmail.com>
2021-10-01 20:49:28 +0800

    feat(vscode): add extensions and config

--- install_vscode_extension.zsh
+++ install_vscode_extension.zsh
@@ -2,3 +2,19 @@
 
 # Jest JavaScript Testing Framework
 code --install-extension orta.vscode-jest
+
+# Icon Theme
+code --install-extension vscode-icons-team.vscode-icons
+
+# Color Theme
+code --install-extension GitHub.github-vscode-theme
+
+# Edit
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension alefragnani.bookmarks
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension foxundermoon.shell-format
+
+# Develop
+code --install-extension eamodio.gitlens
+code --install-extension ms-azuretools.vscode-docker
