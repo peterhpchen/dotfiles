50759193c1ee4e1f22a2e3a4f746bfe9f504956d
peterhpchen <peterhsinpingchen@gmail.com>
2021-11-25 18:39:37 +0800

    remove unused command

--- symlink-setup.zsh
+++ symlink-setup.zsh
@@ -1,17 +1,5 @@
 #!/bin.zsh
 
-# Vim
-sudo ln -sf $PWD/.vim ~/
-
-# Alacritty
-sudo ln -sf $PWD/alacritty ~/.config
-
-# Zsh
-sudo ln -sf $PWD/.zshrc ~/
-
-# Zim
-sudo ln -sf $PWD/.zimrc ~/
-
 # Tmux
 sudo ln -sf $PWD/.tmux ~/
 sudo ln -sf $PWD/.tmux.conf ~/
