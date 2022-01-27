2d1d1712d084ec1fa07ec28d820ff0ffbc257cbc
peterhpchen <peterhsinpingchen@gmail.com>
2022-01-27 17:11:07 +0800

    change symlink file from ext.symlink to symlink.ext

--- nvim/.config/nvim/lua/plugins/configs/lspconfig.symlink.lua
+++ nvim/.config/nvim/lua/plugins/configs/lspconfig.symlink.lua
@@ -1,4 +1,4 @@
-local nvim_lsp = require('lspconfig')
+ local nvim_lsp = require('lspconfig')
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
