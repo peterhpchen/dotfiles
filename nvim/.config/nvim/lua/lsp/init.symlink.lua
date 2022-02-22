23aacc6bb9accce536945e1b9ea91657a9146bac
peterhpchen <peterhsinpingchen@gmail.com>
2022-02-22 17:38:50 +0800

    individual lsp efm config

--- nvim/.config/nvim/lua/lsp/init.symlink.lua
+++ nvim/.config/nvim/lua/lsp/init.symlink.lua
@@ -1,36 +1,27 @@
 local lsp_installer = require("nvim-lsp-installer")
 
 local on_attach = require("lsp/helpers/on_attach")
+local efm = require("lsp/servers/efm")
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local enhance_server_opts = {
+	["efm"] = efm,
+}
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-	local opts = {}
+	local opts = {
+		on_attach = on_attach,
+	}
 
 	-- (optional) Customize the options passed to the server
 	-- if server.name == "tsserver" then
 	--     opts.root_dir = function() ... end
 	-- end
-	if server.name == "efm" then
-		opts = {
-			filetypes = { "lua", "typescriptreact" },
-			init_options = { documentFormatting = true },
-			settings = {
-				languages = {
-					lua = {
-						{ formatCommand = "stylua -s -", formatStdin = true },
-					},
-					typescriptreact = {
-						{
-							formatCommand = "prettier --stdin-filepath ${INPUT}",
-							formatStdin = true,
-						},
-					},
-				},
-			},
-		}
+	if enhance_server_opts[server.name] then
+		enhance_server_opts[server.name](opts)
 	end
 
 	if server.name == "tsserver" then
---
+++ nvim/.config/nvim/lua/lsp/servers/efm/init.symlink.lua
@@ -0,0 +1,26 @@
+local lsp_installer = require("nvim-lsp-installer")
+
+local name = "efm"
+local server_is_found, server = lsp_installer.get_server(name)
+if server_is_found and not server:is_installed() then
+	print("Installing " .. name)
+	server:install()
+end
+
+return function(opts)
+	opts.filetypes = { "lua", "typescriptreact" }
+	opts.init_options = { documentFormatting = true }
+	opts.settings = {
+		languages = {
+			lua = {
+				{ formatCommand = "stylua -s -", formatStdin = true },
+			},
+			typescriptreact = {
+				{
+					formatCommand = "prettier --stdin-filepath ${INPUT}",
+					formatStdin = true,
+				},
+			},
+		},
+	}
+end
