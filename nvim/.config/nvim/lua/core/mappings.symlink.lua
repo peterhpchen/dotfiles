b3c441868c9d8960412dfe48b95d495998da868f
peterhpchen <peterhsinpingchen@gmail.com>
2022-03-24 07:56:35 +0800

    feat(mappings): add info mappings

--- nvim/.config/nvim/lua/core/mappings.symlink.lua
+++ nvim/.config/nvim/lua/core/mappings.symlink.lua
@@ -1,3 +1,13 @@
+local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
+
+-- LSP
+map('n', '<leader>li', '<cmd>LspInfo<cr>', opts)
+map('n', '<leader>lI', '<cmd>LspInstallInfo<cr>', opts)
+
 -- Telescope
-vim.api.nvim_set_keymap('n', '<C-p>', '<cmd>Telescope find_files<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-f>', '<cmd>Telescope live_grep<cr>', { noremap = true })
+map('n', '<C-p>', '<cmd>Telescope find_files<cr>', opts)
+map('n', '<C-f>', '<cmd>Telescope live_grep<cr>', opts)
+
+-- null-ls
+map('n', '<leader>ni', '<cmd>NullLsInfo<cr>', opts)
