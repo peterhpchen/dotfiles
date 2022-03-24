1105961dac9770e96e5d82a168591bac285c1075
peterhpchen <peterhsinpingchen@gmail.com>
2022-03-24 09:27:07 +0800

    feat(lsp): use lspsaga

--- nvim/.config/nvim/lua/core/mappings.symlink.lua
+++ nvim/.config/nvim/lua/core/mappings.symlink.lua
@@ -1,13 +1,24 @@
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
 
--- LSP
-map('n', '<leader>li', '<cmd>LspInfo<cr>', opts)
-map('n', '<leader>lI', '<cmd>LspInstallInfo<cr>', opts)
+-- lsp
+map('n', '<Leader>f', '<Cmd>lua vim.lsp.buf.formatting_sync()<CR>', opts)
+map('n', '<Leader>li', '<Cmd>LspInfo<CR>', opts)
+map('n', '<Leader>lI', '<Cmd>LspInstallInfo<CR>', opts)
 
--- Telescope
-map('n', '<C-p>', '<cmd>Telescope find_files<cr>', opts)
-map('n', '<C-f>', '<cmd>Telescope live_grep<cr>', opts)
+-- lspsaga
+map('n', 'gh', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+map('n', '<Leader>ca', '<Cmd>Lspsaga code_action<CR>', opts)
+map('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+map('n', 'gs', '<Cmd>Lspsaga signature_help<CR>', opts)
+map('n', '<Leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
+map('n', 'gj', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+map('n', 'gk', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
+map('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
+
+-- telescope
+map('n', '<C-p>', '<Cmd>Telescope find_files<CR>', opts)
+map('n', '<C-f>', '<Cmd>Telescope live_grep<CR>', opts)
 
 -- null-ls
-map('n', '<leader>ni', '<cmd>NullLsInfo<cr>', opts)
+map('n', '<Leader>ni', '<Cmd>NullLsInfo<CR>', opts)
