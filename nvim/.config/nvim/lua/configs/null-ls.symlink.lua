b9dd50ffd30e44d2dbd4b24ba64cff07de7e5c32
peterhpchen <peterhsinpingchen@gmail.com>
2022-03-24 15:34:27 +0800

    chore(prettier): replace prettierd to prettier

--- nvim/.config/nvim/lua/configs/null-ls.symlink.lua
+++ nvim/.config/nvim/lua/configs/null-ls.symlink.lua
@@ -5,8 +5,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.shfmt,
-    -- null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.isort,
   },
   on_attach = function(client)
