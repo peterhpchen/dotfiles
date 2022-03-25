6b0f3d612d9bc71984e5ea74cdf35b09238f4e1d
peterhpchen <peterhsinpingchen@gmail.com>
2022-03-25 17:49:33 +0800

    feat(scripts): add setup script

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 ##################################################
-# Keyboard 
+# Keyboard
 ##################################################
 
 # Boost key in speed
+
+echo "Set a shorter Delay until key repeat"
 defaults write -g InitialKeyRepeat -int 1
+
+echo "Set a blazingly fast keyboard repeat rate"
 defaults write -g KeyRepeat -int 1
+
+echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write -g ApplePressAndHoldEnabled -bool false
