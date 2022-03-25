c02c516f2fb2fcd01b776a542443a528ba2ec139
peterhpchen <peterhsinpingchen@gmail.com>
2022-03-25 15:04:22 +0800

    feat(zsh): add enhancd

---
+++ fzf/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "fzf uninstaller"
+
+if [[ "$(command -v fzf)" ]]; then
+  echo "Uninstalling fzf"
+  $(brew --prefix)/opt/fzf/uninstall
+else
+  echo "fzf isn't installed."
+fi
+
+exit 0
