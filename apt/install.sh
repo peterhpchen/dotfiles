6ddbf422add07947d9301a0f6693154c4f413b17
peterhpchen <peterhsinpingchen@gmail.com>
2022-04-01 00:01:49 +0800

    feat(apt): new

---
+++ apt/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "apt installer"
+
+echo "Installing packages from the packages.txt"
+xargs sudo apt-get install <$__dir/packages.txt
+
+exit 0
