ac855fd5dabab17da24ea336cb7c9ab8d7a44dc0
peterhpchen <peterhsinpingchen@gmail.com>
2022-04-01 10:35:57 +0800

    chore(apt): add yes arg

--- apt/install.sh
+++ apt/install.sh
@@ -5,6 +5,6 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "apt installer"
 
 echo "Installing packages from the packages.txt"
-xargs sudo apt-get install <$__dir/packages.txt
+xargs sudo apt-get install -y <$__dir/packages.txt
 
 exit 0
