99370a20ce964accc803f04701b7a0fbe9e1ba24
peterhpchen <peterhsinpingchen@gmail.com>
2022-04-06 23:10:40 +0800

    feat(dircolors): add linux config

--- dircolors/setup.zsh
+++ dircolors/setup.zsh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
-# Mac needs to use gdircolors to set $LS_COLORS
-# https://github.com/gibbling/dircolors
-eval $(gdircolors -b $HOME/.dir_colors)
+if [[ "$(uname)" == "Darwin" ]]; then
+  # Mac needs to use gdircolors to set $LS_COLORS
+  # https://github.com/gibbling/dircolors
+  eval $(gdircolors -b $HOME/.dir_colors)
+elif [[ "$(uname)" == "Linux" ]]; then
+  eval $(dircolors $HOME/.dir_colors)
+fi
