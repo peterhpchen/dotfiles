eeb17af5af39819d8e2bc666019a627f36000a5e
peterhpchen <peterhsinpingchen@gmail.com>
2022-04-07 13:41:32 +0800

    feat(fzf): add to apt

--- fzf/install.sh
+++ fzf/install.sh
@@ -3,8 +3,8 @@
 echo "fzf installer"
 
 echo "Installing fzf"
-# Just for zsh, add fzf
-# https://github.com/junegunn/fzf/blob/master/install
-"$(brew --prefix)"/opt/fzf/install --key-bindings --completion --update-rc --no-bash --no-fish
-
-exit 0
+if [[ "$(uname)" == "Darwin" ]]; then
+  # Just for zsh, add fzf
+  # https://github.com/junegunn/fzf/blob/master/install
+  "$(brew --prefix)"/opt/fzf/install --key-bindings --completion --update-rc --no-bash --no-fish
+fi
