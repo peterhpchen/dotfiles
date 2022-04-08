7f0929dd51770d930d6de875c79a1b24b8e0eaf4
peterhpchen <peterhsinpingchen@gmail.com>
2022-04-08 10:07:11 +0800

    chore(nvim.packer.nvim): format

--- nvim.packer.nvim/install.sh
+++ nvim.packer.nvim/install.sh
@@ -2,10 +2,9 @@
 
 echo "packer.nvim installer"
 
-if [[ ! -d  ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]; then
+if [[ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]; then
   echo "Installing packer.nvim"
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 else
   echo "packer.nvim was installed."
 fi
