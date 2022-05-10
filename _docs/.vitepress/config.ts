c0fd5cb6c8f56dd0d288ae940347f20d0ed1b099
peterhpchen <peterhsinpingchen@gmail.com>
2022-05-10 13:33:01 +0800

    feat(docs): add references

--- _docs/.vitepress/config.ts
+++ _docs/.vitepress/config.ts
@@ -3,21 +3,23 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
 	srcDir: 'src',
 	locales: {
-		// '/': {
-		// 	lang: 'en-US',
-		// 	title: "Peter's Dotfiles"
-		// },
-		'/zh-TW/': {
+		'/': {
+			lang: 'en-US',
+			title: "Peter's Dotfiles"
+		},
+    '/zh-TW/': {
+      selectText: '選擇語言',
+      label: '繁體中文',
 			lang: 'zh-TW',
 			title: 'Dotfiles'
 		}
 	},
 	themeConfig: {
 		locales: {
-			// '/': {
-			// 	selectText: 'Languages',
-			// 	label: 'English'
-			// },
+			'/': {
+				selectText: 'Languages',
+				label: 'English'
+			},
 			'/zh-TW/': {
 				selectText: '選擇語言',
         label: '繁體中文',
@@ -32,6 +34,7 @@ export default defineConfig({
                 { text: '快速上手', link: '/zh-TW/guide/quick-start' },
                 { text: '目錄結構', link: '/zh-TW/guide/directory-structure' },
                 { text: '腳本', link: '/zh-TW/guide/scripts' },
+                { text: '參考資料', link: '/zh-TW/guide/references' },
               ]
             }
           ]
---
+++ _docs/src/zh-TW/guide/references.md
@@ -0,0 +1,5 @@
+# 參考資料
+
+- 在 `dockerfile` 中執行 `zplug install`
+  - [GitHub Issue](https://github.com/zplug/zplug/issues/272#issuecomment-581265819)
+  - [nemanjan00/dev-environment](https://github.com/nemanjan00/dev-environment)
