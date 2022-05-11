3dfdf966c186320b4d20e60ee90d2a6b9c18ac63
peterhpchen <peterhsinpingchen@gmail.com>
2022-05-11 12:17:41 +0800

    fix(rust): cargo install

--- rust/scripts/install.sh
+++ rust/scripts/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+source "$HOME/.cargo/env"
--- rust/zshrc/setup.zsh
+++ rust/zshrc/setup.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-. "$HOME/.cargo/env"
+source "$HOME/.cargo/env"
