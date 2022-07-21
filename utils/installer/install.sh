063bbd94cc69a5b02fe3e6f800212ba305c25f52
peterhpchen <peterhsinpingchen@gmail.com>
2022-07-21 15:04:41 +0800

    fix: nvm default packages

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -21,10 +21,10 @@ if [ ! -f "$NVM_DIR/nvm.sh" ]; then
   git clone --quiet https://github.com/nvm-sh/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout --quiet `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-  . "$NVM_DIR/nvm.sh"
 fi
 
 ln -s "$DOTFILES/nvm/default-packages" "$NVM_DIR"
+. "$NVM_DIR/nvm.sh"
 nvm install 'lts/*'
 
 if [ -d "$DOTFILES" ]; then
