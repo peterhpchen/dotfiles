1d4bc2a668bdf66a0dc7ba8062b192ae3f8969d2
peterhpchen <peterhsinpingchen@gmail.com>
2022-08-05 10:01:47 +0800

    fix(install): wrong path

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -77,5 +77,5 @@ ln -s "$DOTFILES/zsh/.zshenv" "$XDG_CONFIG_HOME/zsh"
 ln -s "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -s "$DOTFILES/zsh/.p10k.zsh" "$XDG_CONFIG_HOME/zsh"
 
-ln -s "$DOTFILES/.editorconfig" "$XDG_CONFIG_HOME"
-ln -s "$DOTFILES/.prettierrc.js" "$XDG_CONFIG_HOME"
+ln -s "$DOTFILES/.editorconfig" "$HOME"
+ln -s "$DOTFILES/.prettierrc.js" "$HOME"
