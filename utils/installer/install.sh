28fba04dbe0f4f79421a3348cb5e07d4ac7709a2
peterhpchen <peterhsinpingchen@gmail.com>
2023-01-12 08:07:03 +0800

    feat(rectangle): set cycle display

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -95,3 +95,6 @@ echo "Set a blazingly fast keyboard repeat rate"
 defaults write -g KeyRepeat -int 1
 echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Rectangle
+defaults write com.knollsoft.Rectangle subsequentExecutionMode -int 1
