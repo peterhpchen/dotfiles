9ec9e2449974788925d2eae4dbc245d54f89e98d
peterhpchen <peterhsinpingchen@gmail.com>
2023-05-15 16:31:00 +0800

    feat(script install): check if can install first

--- .scripts/installer/install.sh
+++ .scripts/installer/install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 DOTFILES="$HOME/.dotfiles"
+
+if [ -d "$DOTFILES" ]; then
+  echo "The \$DOTFILES folder already exists ($DOTFILES). Please remove it."
+  exit 1
+fi
+
 DOTS="$DOTFILES/dots"
 XDG_CONFIG_HOME="$HOME/.config"
 REMOTE=https://github.com/peterhpchen/dotfiles.git
@@ -19,11 +25,6 @@ fi
 if [ ! "$(command -v git)" ]; then
   echo "Git not installed. Installing."
   brew install git
-fi
-
-if [ -d "$DOTFILES" ]; then
-  echo "The \$DOTFILES folder already exists ($DOTFILES). Please remove it."
-  exit 1
 fi
 
 git clone --quiet "$REMOTE" "$DOTFILES"
