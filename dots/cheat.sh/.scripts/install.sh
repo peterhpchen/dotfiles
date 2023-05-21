ef8d1a1079ca48d1302ef4a7d1f1b87c66748379
peterhpchen <peterhsinpingchen@gmail.com>
2023-05-21 11:13:43 +0800

    refactor(cheat.sh): extract xdg

--- dots/cheat.sh/.scripts/install.sh
+++ dots/cheat.sh/.scripts/install.sh
@@ -5,7 +5,7 @@ CHTSH_DIR="$XDG_CONFIG_HOME/cheat.sh"
 mkdir -p "$CHTSH_DIR/bin"
 curl https://cht.sh/:cht.sh >"$CHTSH_DIR/bin/cht.sh"
 chmod +x "$CHTSH_DIR/bin/cht.sh"
-ln -s "$DOTS/cheat.sh/cht.sh.conf" "$CHTSH_DIR"
+ln -s "$DOTS/cheat.sh/XDG_CONFIG_HOME/cht.sh.conf" "$CHTSH_DIR"
 
 # ZSH Tab completion
 mkdir -p "$CHTSH_DIR/zsh-completions"
