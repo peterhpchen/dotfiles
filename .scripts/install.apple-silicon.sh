c9348ede7e5585243bfa202f9c6feb9700402386
Peter Chen <peterhsinpingchen@gmail.com>
2023-09-22 23:15:28 +0800

    Create install script for apple silicon

---
+++ .scripts/install.apple-silicon.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+DOTFILES="$HOME/.dotfiles"
+
+if [ -d "$DOTFILES" ]; then
+  echo "The \$DOTFILES folder already exists ($DOTFILES). Please remove it."
+  exit 1
+fi
+
+DOTS="$DOTFILES/dots"
+XDG_CONFIG_HOME="$HOME/.config"
+REMOTE=https://github.com/peterhpchen/dotfiles.git
+
+if [ ! "$(command -v brew)" ]; then
+  echo "Homebrew not installed. Installing."
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if [ ! "$(command -v git)" ]; then
+  echo "Git not installed. Installing."
+  brew install git
+fi
+
+git clone --quiet "$REMOTE" "$DOTFILES"
+
+. "$DOTS/cheat.sh/.scripts/install.sh"
+. "$DOTS/nvm/.scripts/install.sh"
+. "$DOTS/sdkman/.scripts/install.sh"
+. "$DOTS/ssh/.scripts/install.sh"
+
+. "$DOTS/homebrew/.scripts/setup.sh"
+. "$DOTS/gitui/.scripts/setup.sh"
+
+. "$DOTS/pyenv/.scripts/setup.sh"
+
+. "$DOTS/ripgrep/.scripts/setup.sh"
+. "$DOTS/fzf/.scripts/setup.sh"
+. "$DOTS/neofetch/.scripts/setup.sh"
+
+. "$DOTS/tmux/.scripts/setup.sh"
+
+. "$DOTS/tmuxinator/.scripts/setup.sh"
+
+. "$DOTS/kitty/.scripts/setup.sh"
+
+. "$DOTS/git/.scripts/setup.sh"
+
+. "$DOTS/dircolors/.scripts/setup.sh"
+
+. "$DOTS/bat/.scripts/setup.sh"
+
+. "$DOTS/zsh/.scripts/setup.sh"
+
+. "$DOTS/editorconfig/.scripts/setup.sh"
+. "$DOTS/prettier/.scripts/setup.sh"
+
+. "$DOTS/macos/.scripts/setup.sh"
+. "$DOTS/rectangle/.scripts/setup.sh"
