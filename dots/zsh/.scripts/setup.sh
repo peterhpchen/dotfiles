efeedb79cc1ea2dfb3e27c63e111e89adf17a830
peterhpchen <peterhsinpingchen@gmail.com>
2023-09-25 10:01:15 +0800

    feat(zsh): add zprofile

--- dots/zsh/.scripts/setup.sh
+++ dots/zsh/.scripts/setup.sh
@@ -4,6 +4,7 @@ ln -s "$DOTS/zsh/.zshenv" "$HOME"
 mkdir "$XDG_CONFIG_HOME/zsh"
 ln -s "$DOTS/zsh/XDG_CONFIG_HOME/.zshenv" "$XDG_CONFIG_HOME/zsh"
 ln -s "$DOTS/zsh/XDG_CONFIG_HOME/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -s "$DOTS/zsh/XDG_CONFIG_HOME/.zprofile" "$XDG_CONFIG_HOME/zsh"
 ln -s "$DOTS/zsh/XDG_CONFIG_HOME/.p10k.zsh" "$XDG_CONFIG_HOME/zsh"
 
 cd "$DOTFILES"
