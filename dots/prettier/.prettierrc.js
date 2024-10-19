769e9e818d3b17cb9aabb294cc60c2097e4bfe60
peterhpchen <peterhsinpingchen@gmail.com>
2024-10-19 08:02:38 +0800

    feat(prettier): remove unknown option

--- dots/prettier/.prettierrc.js
+++ dots/prettier/.prettierrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-  editorconfig: true,
+  // // https://github.com/prettier/prettier/issues/15255
+  // // editorconfig options is a Node.js API configuration option object, and not a configuration file option
+  // // Use --no-editorconfig to disable EditorConfig settings in CLI
+  // editorconfig: true,
   singleQuote: true,
   htmlWhitespaceSensitivity: 'strict',
-  // The options below config in EditorConfig, do not config here
+  // // The options below config in EditorConfig, do not config here
   // endOfLine: 'lf', // `end_of_line`
   // useTabs: false, // `indent_style`
   // tabWidth: 2, // `indent_size` / `tab_width`
