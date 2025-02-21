899c1b3a978cc2f7d3134f739cebedc915e4e3ba
peterhpchen <peterhsinpingchen@gmail.com>
2025-02-22 00:47:13 +0800

    feat(sdkman): add springboot completion

--- dots/sdkman/zshenv.zsh
+++ dots/sdkman/zshenv.zsh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
 export SDKMAN_DIR="$XDG_CONFIG_HOME/sdkman"
+
+SPRING_COMPLETION_DIR="$SDKMAN_DIR/candidates/springboot/current/shell-completion/zsh"
+
+if [ -d "$SPRING_COMPLETION_DIR" ]; then
+  export fpath=("$SPRING_COMPLETION_DIR" $fpath)
+fi
